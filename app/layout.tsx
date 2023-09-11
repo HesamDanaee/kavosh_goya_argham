e67ef45451a3e0a0cd4707a12d6c82ef8a186f98
Hesam Danaee <hesamdanaey@outlook.com>
2023-09-11 15:55:11 +0330

    fix shifting layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 export const metadata: Metadata = {
   title: "Kavosh Goya Argham",
   description: "Kavosh Goya Argham company - Accounting",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
--- containers/ServiceTable.tsx
+++ containers/ServiceTable.tsx
@@ -11,7 +11,7 @@ import useWindowSize from "@/hooks/useWindowSize";
 
 export default function ServiceTable() {
   const isMobile = useWindowSize();
-  console.log(isMobile);
+
   return (
     <section className="w-full h-[600px] max-md:h-[1800px] flex flex-col justify-around items-center">
       <h1 className="font-kalame font-[900] text-2xl after_blue relative">
