bf8e22c85943899d9bf2f58e261476385b1b6462
Hesam Danaee <hesamdanaey@outlook.com>
2023-09-11 15:17:57 +0330

    fix window undefined problem

--- hooks/useWindowSize.tsx
+++ hooks/useWindowSize.tsx
@@ -1,16 +1,16 @@
+"use client";
+
 import { useState, useEffect } from "react";
-const isBrowser = () => typeof window !== "undefined";
 
 export default function useWindowSize() {
-  const [isMobile, setIsMobile] = useState(
-    typeof window !== "undefined" ? isBrowser() : false
-  );
+  const [isMobile, setIsMobile] = useState(false);
 
   const checkWindowSize = () => {
     window.innerWidth <= 800 ? setIsMobile(true) : setIsMobile(false);
   };
 
   useEffect(() => {
+    checkWindowSize();
     window.addEventListener("resize", checkWindowSize);
 
     return () => {
